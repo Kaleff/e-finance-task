b70adbe73565b6c949794a09b4d305f608ee9864
Marks Kalijevs <markskalijevs@gmail.com>
2025-11-24 05:08:48 +0200

    [BE] fix: fix email verification for registration

--- backend/app/Http/Requests/RegisterUserRequest.php
+++ backend/app/Http/Requests/RegisterUserRequest.php
@@ -23,7 +23,7 @@ class RegisterUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email:rfc,dns|unique:users,email',
             'password' => 'required|string|min:6|confirmed',
         ];
     }
